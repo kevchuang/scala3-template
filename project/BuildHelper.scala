6c759bdfdb1b49df88e52dd545c63c6797d73072
kevin.chuang <kchuang.pro@gmail.com>
2023-11-01 00:01:37 +0100

    Add commit message to scala-steward

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -1,12 +1,12 @@
 import sbt.Keys.*
 import sbt.*
 
-object BuildHelper:
+object BuildHelper {
   val scala3 = "3.3.1"
 
   def nameSettings: List[Setting[String]] = List(
-    name             := "scala3-template",
-    organization     := "com.kevchuang",
+    name := "scala3-template",
+    organization := "com.kevchuang",
     organizationName := "kevchuang"
   )
 
@@ -14,9 +14,10 @@ object BuildHelper:
     Setting[? >: String & Task[Seq[String]] & Boolean & Seq[TestFramework]]
   ] = List(
     ThisBuild / scalaVersion := scala3,
-    scalacOptions            := ScalaSettings.baseSettings,
+    scalacOptions := ScalaSettings.baseSettings,
     Test / parallelExecution := true,
-    ThisBuild / fork         := true,
-    run / fork               := true,
+    ThisBuild / fork := true,
+    run / fork := true,
     testFrameworks += new TestFramework("weaver.framework.CatsEffect")
   )
+}
